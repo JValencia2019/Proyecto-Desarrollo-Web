a541164635ea12b3cab02cc27cb63c717823c9c8
Jvalencia <valenciastudy@hotmail.es>
2019-11-12 10:31:17 -0500

    version 1.0

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root 'pages#index'
+  get 'pages/productos'
+  get 'pages/ingresar'
+  get 'pages/registrarse'
+
+  get 'pages/contactenos'
+
+  get 'pages/noticias'
+   get 'pages/promociones'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
